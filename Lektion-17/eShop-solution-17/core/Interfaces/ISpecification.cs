76430b893dfb3edc6ebccbb4efab334b4acf0a08
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-04-20 11:11:50 +0200

    Updated Solution for lesson 17

---
+++ Lektion-17/eShop-solution-17/core/Interfaces/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace core.Interfaces;
+
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>>? Predicate { get; }
+    Expression<Func<T, object>>? OrderByAscending { get; }
+    Expression<Func<T, object>>? OrderByDescending { get; }
+    List<Expression<Func<T, object>>> Includes { get; }
+    // Paginerings egenskaper...
+    int Skip { get; }
+    int Take { get; }
+    bool IsPaginationActivated { get; }
+    IQueryable<T> ApplyPredicate(IQueryable<T> query);
+}
